397eff8ba2231f28c5f2df436b8bcea1b5faf1c7
zacharygt <2081871068@qq.com>
2023-09-12 18:29:17 +0800

    Site updated: 2023-09-12 18:29:15

--- js/lib/highlight.js
+++ js/lib/highlight.js
@@ -1,27 +1,33 @@
 mixins.highlight = {
-    data() {
-        return { copying: false };
+  data() {
+    return {
+      copying: false
+    };
+  },
+  created() {
+    hljs.configure({
+      ignoreUnescapedHTML: true
+    });
+    this.renderers.push(this.highlight);
+  },
+  methods: {
+    sleep(delay) {
+      return new Promise(resolve => setTimeout(resolve, delay));
     },
-    created() {
-        hljs.configure({ ignoreUnescapedHTML: true });
-        this.renderers.push(this.highlight);
-    },
-    methods: {
-        sleep(delay) {
-            return new Promise(resolve => setTimeout(resolve, delay));
-        },
-        highlight() {
-            let codes = document.querySelectorAll("pre");
-            for (let i of codes) {
-                let code = i.textContent;
-                let language = [...i.classList, ...i.firstChild.classList][0] || "plaintext";
-                let highlighted;
-                try {
-                    highlighted = hljs.highlight(code, { language }).value;
-                } catch {
-                    highlighted = code;
-                }
-                i.innerHTML = `
+    highlight() {
+      let codes = document.querySelectorAll("pre");
+      for (let i of codes) {
+        let code = i.textContent;
+        let language = [...i.classList, ...i.firstChild.classList][0] || "plaintext";
+        let highlighted;
+        try {
+          highlighted = hljs.highlight(code, {
+            language
+          }).value;
+        } catch {
+          highlighted = code;
+        }
+        i.innerHTML = `
                     <div class="code-content hljs">${highlighted}</div>
                     <div class="language">${language}</div>
                     <div class="copycode">
@@ -29,19 +35,21 @@ mixins.highlight = {
                         <i class="fa-solid fa-clone fa-fw"></i>
                     </div>
                 `;
-                let content = i.querySelector(".code-content");
-                hljs.lineNumbersBlock(content, { singleLine: true });
-                let copycode = i.querySelector(".copycode");
-                copycode.addEventListener("click", async () => {
-                    if (this.copying) return;
-                    this.copying = true;
-                    copycode.classList.add("copied");
-                    await navigator.clipboard.writeText(code);
-                    await this.sleep(1000);
-                    copycode.classList.remove("copied");
-                    this.copying = false;
-                });
-            }
-        },
-    },
+        let content = i.querySelector(".code-content");
+        hljs.lineNumbersBlock(content, {
+          singleLine: true
+        });
+        let copycode = i.querySelector(".copycode");
+        copycode.addEventListener("click", async () => {
+          if (this.copying) return;
+          this.copying = true;
+          copycode.classList.add("copied");
+          await navigator.clipboard.writeText(code);
+          await this.sleep(1000);
+          copycode.classList.remove("copied");
+          this.copying = false;
+        });
+      }
+    }
+  }
 };
